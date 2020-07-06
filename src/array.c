3bf8b0abe302ae53a91cc57b9788d1704afc2843
ユーザー <user@yuza-no-MacBook-Air.local>
2020-07-07 01:05:39 +0900

    revised

--- src/array.c
+++ src/array.c
@@ -4,15 +4,25 @@ int n;
 int k;
 int A[100000];
 
-
+ 
 int main(){
-  int i, lb, ub;
+  int i ; 
+  int lb ;
+  int ub ;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  lb = 0;
+  ub = n;
+  while(ub - lb > 1){
+      int mid = (ub+lb)/2;
+      if(A[mid] >= k)
+          ub = mid;
+      else 
+          lb = mid;
+  }
+  printf("%d\n",ub);
+  
   return 0;
 }

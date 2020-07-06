3cc0468474492a1a5681081ed3e2a474053d50ba
ユーザー <user@yuza-no-MacBook-Air.local>
2020-07-07 01:09:25 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,15 @@ int n;
 int k;
 int A[100000];
 
+int p(unsigned int x){
+    int sum,i;
+    sum = 0;
+    for(i = 0;i< n;i++){
+        sum = sum + (A[i]+(x-1))/x;
+    }
+    return sum <= k;
+}
+
 
 int main(){
   int i, lb, ub;
@@ -11,7 +20,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1000000000; 
+  while(ub - lb > 1){
+      int mid = (lb+ ub)/2;
+      if(p(mid)){
+          ub = mid;
+      }
+      else{
+          lb = mid;
+      }
+  }
+  printf("%d\n",ub);
   return 0;
 }

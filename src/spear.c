1790f72e569a2945106e972ede9682d00da351b4
ユーザー <user@yuza-no-MacBook-Air.local>
2020-07-07 01:09:54 +0900

    revised

--- src/spear.c
+++ src/spear.c
@@ -4,14 +4,32 @@ int n;
 int k;
 int A[100000];
 
+int p(unsigned int x){
+    int sum,i;
+    sum = 0;
+    for(i = 0;i< n;i++){
+        sum = sum +(A[i]/x);
+    }
+    return sum >= k;
+}
 
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+    }
+    lb = 0;
+    ub = 1000000000;
+    while(ub - lb > 1){
+      int mid = (lb+ ub)/2;
+      if(p(mid)){
+          lb = mid;
+      }
+      else{
+          ub = mid;
+      }
   }
-
-
+  printf("%d\n",lb);
   return 0;
-}
+  }
